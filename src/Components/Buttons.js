82f930d4bff186518a25b577e11b51b78776e24e
Victor <84178696+victorcodess@users.noreply.github.com>
2022-11-06 16:56:25 +0100

    created state for features

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import alpacaDatabase from "../alpacaData";
 
 const Buttons = (props) => {
-    return (
-        <div className="buttons">
-            <h1 className="header">ACCESSORISE THE ALPACA'S</h1>
-            <div className="buttons">
-                <button>Hair</button>
-            </div>
-        </div>
-    )
-}
+  const [feature, setFeature] = useState({});
 
-export default Buttons 
+  const handleSetDir = (feature) => {
+    setFeature(() => feature);
+  };
+
+  return (
+    <div className="buttons">
+      <div className="feature-buttons">
+        <h2 className="header">ACCESSORISE THE ALPACA'S</h2>
+        {alpacaDatabase.map((feature, index) => (
+          <button className="feature" onClick={() => handleSetDir(feature)}>
+            {feature.label}
+          </button>
+        ))}
+      </div>
+
+      <div className="style-buttons">
+        {Object.keys(feature).length
+          ? feature.items.map((item, index) => (
+              <div key={index} className="style-grid">
+                <button className="style">{item.label}</button>
+              </div>
+            ))
+          : null}
+      </div>
+    </div>
+  );
+};
+
+export default Buttons;
